a145ac275d195246173689431d534a8fef2e1008
rahuldcosta <rahuldc999@gmail.com>
2015-06-25 20:07:31 +0530

    main updated

--- ElevatorSE/src/elevatorse2015/Main.java
+++ ElevatorSE/src/elevatorse2015/Main.java
@@ -8,16 +8,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int ff;
 		System.out.println("The Elevator Program");
 		
 		Elevator e = new Elevator();
 		
 		e.setCurrent_pos(0);
 		
+		e.ed.closedoor();
+		
 		Floor f = new Floor();
 		
 		f.setFloor_no(0);
+		
+		
+		f.fb.display();
+		ff=f.fb.selectFloor();
+		e.getCurrent_pos();
 		
 
 	}
