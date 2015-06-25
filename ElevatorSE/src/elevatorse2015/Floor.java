aa58e4b11472cd997c74ecaba7fd1fe790941ad7
piyu2115 <priyushet@gmail.com>
2015-06-25 17:11:39 +0530

    Commit floor $ floor button

--- ElevatorSE/src/elevatorse2015/Floor.java
+++ ElevatorSE/src/elevatorse2015/Floor.java
@@ -2,8 +2,17 @@ package elevatorse2015;
 
 public class Floor {
 
-	public Floor(){
-		
+	private int floor_no;
+    FloorButton fb=new FloorButton();
+     
+  
+
+	public int getFloor_no() {
+		return floor_no;
+	}
+
+	public void setFloor_no(int floor_no) {
+		this.floor_no = floor_no;
 	}
 	
 }
--- ElevatorSE/src/elevatorse2015/FloorButton.java
+++ ElevatorSE/src/elevatorse2015/FloorButton.java
@@ -1,5 +1,24 @@
 package elevatorse2015;
 
+import java.util.Scanner;
+
 public class FloorButton {
 
+	public FloorButton(){
+		display();
+	}
+	
+	public int selectFloor(){
+		Scanner in=new Scanner(System.in);
+	    int floor=in.nextInt();
+	    return floor;
+	}
+	
+	public void display(){
+		System.out.println("Select Floor you want to go");
+		System.out.println("1	2	3");
+		System.out.println("4	5	6");
+	
+	}
+	
 }
