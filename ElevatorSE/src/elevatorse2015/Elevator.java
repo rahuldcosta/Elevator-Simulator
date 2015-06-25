49e8b415c624e387e2bd4d6a376a420298035fbd
rahuldcosta <rahuldc999@gmail.com>
2015-06-25 16:44:52 +0530

    Our Elevator Begins

---
+++ ElevatorSE/src/elevatorse2015/Elevator.java
@@ -0,0 +1,9 @@
+package elevatorse2015;
+
+public class Elevator {
+
+	public Elevator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ElevatorSE/src/elevatorse2015/ElevatorDoor.java
@@ -0,0 +1,9 @@
+package elevatorse2015;
+
+public class ElevatorDoor {
+
+	public ElevatorDoor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
