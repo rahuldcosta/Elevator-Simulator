592993d8de2d4341c12fcba4f94000f6f2949df1
rahuldcosta <rahuldc999@gmail.com>
2015-07-09 13:28:56 +0530

    Update Main.java

--- ElevatorSE/src/elevatorse2015/Main.java
+++ ElevatorSE/src/elevatorse2015/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		//Puller
+		//Puller Now
 		//Design Patterns
 		//Design Pattern :- Classes Participating in The Design Pattern
 //		1.State:- ElevatorDoorContext,State,OpenDoor,CLoseDoor
